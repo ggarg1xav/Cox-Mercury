257b2aa4ab71fee9e6e38c8469be0a1a6efaf487
ggarg1xav <ggarg1@xavient.com>
2016-12-13 10:44:27 +0530

    Update Test_Login.java

--- src/test/java/com/xavient/test/script/Test_Login.java
+++ src/test/java/com/xavient/test/script/Test_Login.java
@@ -1,4 +1,4 @@
-package com.xavient.framework.COX_Test_Suite;
+package com.xavient.test.script;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,71 +12,56 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.xavient.framework.util.Properties_Reader;
+import com.xavient.pages.Login;
+import com.xavient.util.Properties_Reader;
 
 public class Test_Login {
-	
 
 	WebDriver driver;
 
-	    /**
-	     * This function will execute before each Test tag in testng.xml
-	     * @param browser
-	     * @throws Exception
-	     */
-	    @BeforeTest
-	    @Parameters({"browser"})
- public void Before_Test(@Optional("Chrome") String browser) throws Exception{
-	    	String path=System.getProperty("user.dir");
-	    	System.out.println(path + browser + "Nishant Makkar");
-	        if(browser.equalsIgnoreCase("firefox")){
-	        //create firefox instance
-	            driver = new FirefoxDriver();
-	        }
-	      
-	        else if(browser.equalsIgnoreCase("Chrome")){
-	            //set path to chromedriver.exe
-	        	System.out.println(path+"\\Drivers_executable\\chromedriver.exe");
-	            System.setProperty("webdriver.chrome.driver",path+"\\Drivers_executable\\chromedriver.exe");
-	           
-	            driver = new ChromeDriver();
-	        }
-	       
-	                else if(browser.equalsIgnoreCase("ie")){
-	                  
-	                    System.setProperty("webdriver.ie.driver",path+"\\COX_Test\\Drivers_executable\\IEDriverServer.exe");
-	                   
-	                    driver = new InternetExplorerDriver();
-	                }
-	        
-	        else{
-	            //If no browser passed throw exception
-	            throw new Exception("Browser is not correct");
-	        }
-	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    	System.out.println("------Before Test------");
-	    }
-	  @Test
-	    public void login() throws Exception{
-		  driver.get(Properties_Reader.readProperty("URL"));
-	        Login.userName(driver).sendKeys(Properties_Reader.readProperty("Username"));
-	        Login.Password(driver).sendKeys(Properties_Reader.readProperty("Password"));
-	    	Login.login_button(driver).click();
-	    }
+	@BeforeTest
+	@Parameters({ "browser" })
+	public void Before_Test(@Optional("Chrome") String browser) throws Exception {
+		String path = System.getProperty("user.dir");
+		System.out.println(path);
+		if (browser.equalsIgnoreCase("firefox")) {
+			// create firefox instance
+			driver = new FirefoxDriver();
+		}
 
-	 
-	    
-	    @AfterTest
-	 public void After_Test(){
-	    	
-	    	System.out.println("------End Test------");
-	    	driver.close();
-	    }
-	  
-	    
-	  
+		else if (browser.equalsIgnoreCase("Chrome")) {
+			// set path to chromedriver.exe
+			System.out.println(path + "\\main\\resources\\Drivers_executable\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", path + "\\src\\main\\resources\\Drivers_executable\\chromedriver.exe");
+
+			driver = new ChromeDriver();
+		}
+
+		else if (browser.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", path + "\\rc\\main\\resources\\Drivers_executable\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+
+		else {
+			// If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("------Before Test------");
+	}
+
+	@Test
+	public void login() throws Exception {
+		driver.get(Properties_Reader.readProperty("URL"));
+		Login.userName(driver).sendKeys(Properties_Reader.readProperty("Username"));
+		Login.Password(driver).sendKeys(Properties_Reader.readProperty("Password"));
+		Login.login_button(driver).click();
+	}
+
+	@AfterTest
+	public void After_Test() {
+		System.out.println("------End Test------");
+		driver.close();
+	}
+
 }
-	    
-	       
-
-
