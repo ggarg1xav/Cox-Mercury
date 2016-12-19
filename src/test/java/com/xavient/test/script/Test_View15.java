9f80428b4f1e7cc27ee64ffe4d37e2c86124f22e
csingh5 <csingh5@XAV-101000616.xavient.com>
2016-12-19 16:44:59 +0530

    Remove unused import

--- src/test/java/com/xavient/test/script/Test_View15.java
+++ src/test/java/com/xavient/test/script/Test_View15.java
@@ -1,17 +1,12 @@
 package com.xavient.test.script;
 
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
