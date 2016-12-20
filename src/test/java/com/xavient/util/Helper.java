fba1a28bfe3fe87b8d63bcdaa4d620d90b55c841
Guneet Garg <ggarg1@xavient.com>
2016-12-20 13:10:05 +0530

    Handling table drill-down operation

--- src/test/java/com/xavient/util/Helper.java
+++ src/test/java/com/xavient/util/Helper.java
@@ -239,4 +239,23 @@ public class Helper {
 		}
 
 	}
+	
+	/**
+	 * Handling table drill-down operation.
+	 * @author guneet
+	 */	
+	public void validate_drilldown(String view2Table, String view2DrillStart, String view2DrillEnd, WebDriver driver) {
+		waitForBrowserToLoadCompletely(driver);
+		int count = driver.findElements(By.xpath(view2Table)).size();
+		int i = 1;
+		if (count > 0) {
+			List<WebElement> dr = driver.findElements(By.xpath(view2DrillStart + i + view2DrillEnd));
+			while (dr.get(dr.size() - 1).getAttribute("class").toString()
+					.equalsIgnoreCase("treegrid-expander treegrid-expander-collapsed drilling")) {
+				dr.get(dr.size() - 1).click();
+				i++;
+				dr = driver.findElements(By.xpath(view2DrillStart + i + view2DrillEnd));
+			}
+		}
+	}	
 }
