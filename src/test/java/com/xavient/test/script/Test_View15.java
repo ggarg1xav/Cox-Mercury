454fd5dc7177be5322039d55c9135c61163d7011
csingh5 <csingh5@XAV-101000616.xavient.com>
2016-12-20 10:30:54 +0530

    Commit latest code

--- src/test/java/com/xavient/test/script/Test_View15.java
+++ src/test/java/com/xavient/test/script/Test_View15.java
@@ -78,6 +78,7 @@ public class Test_View15 extends BaseClass implements DashBoardView {
 			helper.validate_table_columns(view15_current_data_table, driver, "", "Test_View15", "view15_current_data_table");
 			helper.validate_table_names(driver.findElement(view15_Half_Hour_data), "Test_View15", "view15_view15_Half_Hour_data_details");
 			helper.validate_table_columns(view15_Half_Hour_data_table, driver, "", "Test_View15", "view15_view15_Half_Hour_data_table");
+			System.out.println("Test");
 	}
 	
 	/**
