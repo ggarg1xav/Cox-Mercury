242336ce297349b2cac9293527112c12743a35c5
csingh5 <csingh5@XAV-101000616.xavient.com>
2016-12-22 11:47:25 +0530

    Signed-off-by: csingh5 <csingh5@XAV-101000616.xavient.com>

--- src/test/java/com/xavient/test/script/Test_View15.java
+++ src/test/java/com/xavient/test/script/Test_View15.java
@@ -12,12 +12,11 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import com.xavient.pages.DashBoardView;
 import com.xavient.util.BaseClass;
 import com.xavient.util.Helper;
