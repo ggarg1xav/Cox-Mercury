78d9df31833399ceda47dffb9566bc66e9997739
csingh5 <csingh5@XAV-101000616.xavient.com>
2016-12-29 14:20:22 +0530

    Signed-off-by: csingh5 <csingh5@XAV-101000616.xavient.com>

--- src/test/java/com/xavient/util/BaseClass.java
+++ src/test/java/com/xavient/util/BaseClass.java
@@ -75,7 +75,7 @@ public class BaseClass implements DashBoardView{
 			}
 			driver.findElement(IECertificate).click();
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(10000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
